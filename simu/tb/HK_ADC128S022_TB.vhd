4672342789960dfa15e6d41a43701c79d286d6e1
KenjiDeLaRosa <kenji.delarosa@alten.com>
2024-01-17 09:28:41 +0100

    simu: beautify

--- simu/tb/HK_ADC128S022_TB.vhd
+++ simu/tb/HK_ADC128S022_TB.vhd
@@ -36,142 +36,142 @@
 -- -------------------------------------------------------------------------------------------------------------
 
 
-LIBRARY ieee;
-USE ieee.std_logic_1164.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --USE ieee.numeric_std.ALL;
 
-ENTITY HK_ADC128S022_TB IS
-END HK_ADC128S022_TB;
+entity HK_ADC128S022_TB is
+end HK_ADC128S022_TB;
 
-ARCHITECTURE behavior OF HK_ADC128S022_TB IS
+architecture behavior of HK_ADC128S022_TB is
 
-    -- Component Declaration for the Unit Under Test (UUT)
+  -- Component Declaration for the Unit Under Test (UUT)
 
-    COMPONENT ADC128Sxxx
-    PORT(
-         Reset_n : IN  std_logic;
-         clk : IN  std_logic;
-         channel_address : IN  std_logic_vector(2 downto 0);
-         Output_value : OUT  std_logic_vector(11 downto 0);
-         Start : IN  std_logic;
-         Done : OUT  std_logic;
-         ADC_Sclk : OUT  std_logic;
-         ADC_Dout : IN  std_logic;
-         ADC_Din : OUT  std_logic;
-         ADC_Cs_n : OUT  std_logic
-        );
-    END COMPONENT;
+  component ADC128Sxxx
+    port(
+      Reset_n         : in  std_logic;
+      clk             : in  std_logic;
+      channel_address : in  std_logic_vector(2 downto 0);
+      Output_value    : out std_logic_vector(11 downto 0);
+      Start           : in  std_logic;
+      Done            : out std_logic;
+      ADC_Sclk        : out std_logic;
+      ADC_Dout        : in  std_logic;
+      ADC_Din         : out std_logic;
+      ADC_Cs_n        : out std_logic
+      );
+  end component;
 
-   COMPONENT adc128s102
-   PORT (
-        SCLK  : IN  std_ulogic := 'U';
-        CSNeg : IN  std_ulogic := 'U';
-        DIN   : IN  std_ulogic := 'U';
-        VA    : IN  real       := 5.0;
-        IN0   : IN  real       := 0.0;
-        IN1   : IN  real       := 0.0;
-        IN2   : IN  real       := 0.0;
-        IN3   : IN  real       := 0.0;
-        IN4   : IN  real       := 0.0;
-        IN5   : IN  real       := 0.0;
-        IN6   : IN  real       := 0.0;
-        IN7   : IN  real       := 0.0;
-        DOUT  : OUT std_ulogic := 'U'
-        );
-  END COMPONENT;
+  component adc128s102
+    port (
+      SCLK  : in  std_ulogic := 'U';
+      CSNeg : in  std_ulogic := 'U';
+      DIN   : in  std_ulogic := 'U';
+      VA    : in  real       := 5.0;
+      IN0   : in  real       := 0.0;
+      IN1   : in  real       := 0.0;
+      IN2   : in  real       := 0.0;
+      IN3   : in  real       := 0.0;
+      IN4   : in  real       := 0.0;
+      IN5   : in  real       := 0.0;
+      IN6   : in  real       := 0.0;
+      IN7   : in  real       := 0.0;
+      DOUT  : out std_ulogic := 'U'
+      );
+  end component;
 
 
-   --Inputs
-   signal Reset_n : std_logic := '0';
-   signal clk : std_logic := '0';
-   signal channel_address : std_logic_vector(2 downto 0) := (others => '0');
-   signal Start : std_logic := '0';
-   signal ADC_Dout : std_logic := '0';
+  --Inputs
+  signal Reset_n         : std_logic                    := '0';
+  signal clk             : std_logic                    := '0';
+  signal channel_address : std_logic_vector(2 downto 0) := (others => '0');
+  signal Start           : std_logic                    := '0';
+  signal ADC_Dout        : std_logic                    := '0';
 
   --Outputs
-   signal Output_value : std_logic_vector(11 downto 0);
-   signal Done : std_logic;
-   signal ADC_Sclk : std_logic;
-   signal ADC_Din : std_logic;
-   signal ADC_Cs_n : std_logic;
+  signal Output_value : std_logic_vector(11 downto 0);
+  signal Done         : std_logic;
+  signal ADC_Sclk     : std_logic;
+  signal ADC_Din      : std_logic;
+  signal ADC_Cs_n     : std_logic;
 
-   -- Clock period definitions
-   constant clk_period : time := 312.5 ns; --3.2MHz
+  -- Clock period definitions
+  constant clk_period : time := 312.5 ns;  --3.2MHz
 
-BEGIN
+begin
 
   -- Instantiate the Unit Under Test (UUT)
-   uut: ADC128Sxxx PORT MAP (
-          Reset_n => Reset_n,
-          clk => clk,
-          channel_address => channel_address,
-          Output_value => Output_value,
-          Start => Start,
-          Done => Done,
-          ADC_Sclk => ADC_Sclk,
-          ADC_Dout => ADC_Dout,
-          ADC_Din => ADC_Din,
-          ADC_Cs_n => ADC_Cs_n
-        );
+  uut : ADC128Sxxx port map (
+    Reset_n         => Reset_n,
+    clk             => clk,
+    channel_address => channel_address,
+    Output_value    => Output_value,
+    Start           => Start,
+    Done            => Done,
+    ADC_Sclk        => ADC_Sclk,
+    ADC_Dout        => ADC_Dout,
+    ADC_Din         => ADC_Din,
+    ADC_Cs_n        => ADC_Cs_n
+    );
 
 
-  ADC_Component: adc128s102
-   PORT MAP(
-        SCLK  => ADC_Sclk,
-        CSNeg => ADC_Cs_n,
-        DIN   => ADC_Din,
-        VA    => 5.0,
-        IN0   => 0.1,
-        IN1   => 1.0,
-        IN2   => 2.0,
-        IN3   => 3.0,
-        IN4   => 4.0,
-        IN5   => 5.0,
-        IN6   =>1.2,
-        IN7   => 2.5,
-        DOUT  => ADC_Dout
-        );
+  ADC_Component : adc128s102
+    port map(
+      SCLK  => ADC_Sclk,
+      CSNeg => ADC_Cs_n,
+      DIN   => ADC_Din,
+      VA    => 5.0,
+      IN0   => 0.1,
+      IN1   => 1.0,
+      IN2   => 2.0,
+      IN3   => 3.0,
+      IN4   => 4.0,
+      IN5   => 5.0,
+      IN6   => 1.2,
+      IN7   => 2.5,
+      DOUT  => ADC_Dout
+      );
 
-   -- Clock process definitions
-   clk_process :process
-   begin
+  -- Clock process definitions
+  clk_process : process
+  begin
     clk <= '0';
     wait for clk_period/2;
     clk <= '1';
     wait for clk_period/2;
-   end process;
+  end process;
 
 
 
-   -- Stimulus process
-   stim_proc: process
-   begin
-      -- hold reset state for 100 ns.
-      wait for 100 ns;
+  -- Stimulus process
+  stim_proc : process
+  begin
+    -- hold reset state for 100 ns.
+    wait for 100 ns;
 
     Reset_n <= '1';
 
-      wait for clk_period*10;
+    wait for clk_period*10;
 
-      -- insert stimulus here
+    -- insert stimulus here
     channel_address <= "000";
-    start <= '1';
+    start           <= '1';
     -- attente 2 clk
     wait for clk_period*2;
-    start <='0';
+    start           <= '0';
     --attente pour tempo 100kHz
     wait for clk_period*100;
 
     channel_address <= "100";
     wait for clk_period*10;
-    start <= '1';
+    start           <= '1';
 
 
 
-      wait;
-   end process;
+    wait;
+  end process;
 
-END;
+end;
