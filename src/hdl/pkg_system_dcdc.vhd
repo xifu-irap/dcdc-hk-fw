3d35b18adb385c1960e0bd55375954658b6088e8
KenjiDeLaRosa <kenji.delarosa@alten.com>
2024-01-23 07:47:50 +0100

    system: update the pkg_system_dcdc.vhd with the pkg_spi.vhd content.

--- src/hdl/pkg_system_dcdc.vhd
+++ src/hdl/pkg_system_dcdc.vhd
@@ -36,9 +36,61 @@ use ieee.std_logic_1164.all;
 package pkg_system_dcdc is
 
 
-  type register_ADC is array (0 to 7) of std_logic_vector (11 downto 0);
 
-  type register_ADC16 is array (0 to 7) of std_logic_vector (15 downto 0);
+  ---------------------------------------------------------------------
+  -- IO
+  ---------------------------------------------------------------------
+
+  -- IO SPI
+  ---------------------------------------------------------------------
+  -- user-defined : Input Delay. Number of delay clock periods for miso signal
+  constant pkg_IO_SPI_MISO_DELAY : positive := 1; -- must be >= 0
+  -- user-defined: Input resynchronization stage. Number of resync stage
+  constant pkg_IO_SPI_MISO_RESYNC_DELAY : positive := 2; -- must be >= 2
+  -- auto-computed: Number of delay clock periods for miso signal between the pads side -> user side
+  constant pkg_IO_SPI_MISO_LATENCY : positive := pkg_IO_SPI_MISO_DELAY + pkg_IO_SPI_MISO_RESYNC_DELAY;
+  -- user-defined : Number of delay clock periods for mosi signal between the user side -> pads side
+  constant pkg_IO_SPI_MOSI_DELAY : positive := 1; -- must be >= 1
+
+  -- user-defined : usb_clock frequency
+  constant pkg_USB_SYSTEM_FREQUENCY_HZ : positive := 100_800_000;
+
+  ---------------------------------------------------------------------
+  -- spi_device_select
+  ---------------------------------------------------------------------
+   -- hardcoded : Number of clock periods for mosi signal (spi_master) between the spi devices and the module output
+  constant pkg_ADC_SPI_DEVICE_SELECT_MOSI_DELAY : positive := 1;
+
+  ---------------------------------------------------------------------
+  -- io
+  ---------------------------------------------------------------------
+   -- auto-computed : Number of clock periods for mosi signal between the user side -> pads side
+  constant pkg_IO_ADC_MOSI_DELAY : positive := pkg_IO_SPI_MOSI_DELAY;
+   -- auto-computed : Number of clock periods for miso signal between the pads side -> user side
+  constant pkg_IO_ADC_MISO_DELAY : positive := pkg_IO_SPI_MISO_DELAY;
+
+
+  -------------------------------------------------------------------
+  -- AMC7823
+  --   .see: https://www.analog.com/en/analog-dialogue/articles/introduction-to-spi-interface.html
+  -------------------------------------------------------------------
+  -- user-defined : SPI clock polarity
+  constant pkg_ADC_SPI_CPOL                 : std_logic := '0';
+  -- user-defined : SPI clock phase
+  constant pkg_ADC_SPI_CPHA                 : std_logic := '1';
+  -- auto-computed : input clock frequency of the module (expressed in Hz). (possible values: ]2*g_SPI_FREQUENCY_MAX_HZ: max_integer_value])
+  constant pkg_ADC_SPI_SYSTEM_FREQUENCY_HZ  : positive  := pkg_USB_SYSTEM_FREQUENCY_HZ;
+  -- user-defined : spi output clock frequency to generate (expressed in Hz)
+  constant pkg_ADC_SPI_SPI_FREQUENCY_MAX_HZ : positive  := 6_000_000; -- 12 MHz max
+  -- user-defined : Number of clock period for mosi signal between the state machine output to the output ports (spi_master)
+  -- (possible values [0;max_integer_value[)
+  constant pkg_ADC_SPI_MOSI_DELAY           : natural   := 0;
+  -- auto-computed : Number of clock period for miso signal by considering the FPGA loopback delay
+  -- (the external device delay is not taken into account): FSM (spi_master) -> IO (out) -> IO (IN). (possible values [0;max_integer_value[)
+  constant pkg_ADC_SPI_MISO_DELAY           : natural   := pkg_ADC_SPI_MOSI_DELAY +
+                                                           pkg_ADC_SPI_DEVICE_SELECT_MOSI_DELAY +
+                                                           pkg_IO_ADC_MOSI_DELAY +
+                                                           pkg_IO_ADC_MISO_DELAY;
 
 
 end pkg_system_dcdc;
