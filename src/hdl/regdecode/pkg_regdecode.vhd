505d33f5a3540f2c2c038f6e8fa9d0bca14b77b4
KenjiDeLaRosa <kenji.delarosa@alten.com>
2024-02-20 08:58:01 +0100

    version: update Firmware ID

--- src/hdl/regdecode/pkg_regdecode.vhd
+++ src/hdl/regdecode/pkg_regdecode.vhd
@@ -41,7 +41,7 @@ package pkg_regdecode is
   --
   ---------------------------------------------------------------------
   -- user-defined: Firmware ID Value
-  constant pkg_FIRMWARE_ID_VALUE : integer := 2;
+  constant pkg_FIRMWARE_ID_VALUE : integer := 3;
 
   -- user-defined: FIRMWARE name (character3)
   constant pkg_FIRMWARE_NAME_CHAR3 : character := 'D';  -- ascii character
